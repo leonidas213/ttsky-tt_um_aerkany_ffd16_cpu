c0ee25705d0dd4df951661df173684f18230886d
leonidas213 <bberkanticoz@hotmail.com>
2026-05-05 00:24:01 +0300

    Sim fix

--- src/tt_um_remedy_cpu.v
+++ src/tt_um_remedy_cpu.v
@@ -1935,12 +1935,19 @@ module qspi_memory_interface (
     localparam integer INIT_CLK_HZ      = 50000000;
     localparam integer INIT_WAIT_BITS   = 22;
 
-    localparam [INIT_WAIT_BITS-1:0] INIT_POWER_WAIT_CYCLES      = (INIT_CLK_HZ / 1000) * 10; // 10 ms
-    localparam [INIT_WAIT_BITS-1:0] INIT_RESET_WAIT_CYCLES      = (INIT_CLK_HZ / 1000) * 2;  //  2 ms
-    localparam [INIT_WAIT_BITS-1:0] INIT_FLASH_SR2_WAIT_CYCLES  = (INIT_CLK_HZ / 1000) * 40; // 40 ms
-    localparam [INIT_WAIT_BITS-1:0] INIT_FLASH_CONT_WAIT_CYCLES = (INIT_CLK_HZ / 1000) * 1;  //  1 ms
-    localparam [INIT_WAIT_BITS-1:0] INIT_CMD_GAP_CYCLES         = (INIT_CLK_HZ / 1000000) * 5 + 4; // >= about 5 us
-
+    `ifdef COCOTB_SIM_FAST_INIT
+    localparam [INIT_WAIT_BITS-1:0] INIT_POWER_WAIT_CYCLES      = 22'd82;
+    localparam [INIT_WAIT_BITS-1:0] INIT_RESET_WAIT_CYCLES      = 22'd82;
+    localparam [INIT_WAIT_BITS-1:0] INIT_FLASH_SR2_WAIT_CYCLES  = 22'd82;
+    localparam [INIT_WAIT_BITS-1:0] INIT_FLASH_CONT_WAIT_CYCLES = 22'd82;
+    localparam [INIT_WAIT_BITS-1:0] INIT_CMD_GAP_CYCLES         = 22'd82;
+`else
+    localparam [INIT_WAIT_BITS-1:0] INIT_POWER_WAIT_CYCLES      = (INIT_CLK_HZ / 1000) * 10;
+    localparam [INIT_WAIT_BITS-1:0] INIT_RESET_WAIT_CYCLES      = (INIT_CLK_HZ / 1000) * 2;
+    localparam [INIT_WAIT_BITS-1:0] INIT_FLASH_SR2_WAIT_CYCLES  = (INIT_CLK_HZ / 1000) * 40;
+    localparam [INIT_WAIT_BITS-1:0] INIT_FLASH_CONT_WAIT_CYCLES = (INIT_CLK_HZ / 1000) * 1;
+    localparam [INIT_WAIT_BITS-1:0] INIT_CMD_GAP_CYCLES         = (INIT_CLK_HZ / 1000000) * 5 + 4;
+`endif
     function [INIT_WAIT_BITS-1:0] init_gap_cycles;
         input [3:0] stp;
         begin
